816dc7e96d7ee73f6530dfa8a5258446a3ab5a25
bulela <bulelantutu@gmail.com>
2020-09-21 11:02:40 +0200

    First day's work

---
+++ day1.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+class_name = "class 2"
+
+print("I was coded in " + class_name)
+
+class_students = "19"
+
+print("There are " + class_students + " students in " + class_name)
+
