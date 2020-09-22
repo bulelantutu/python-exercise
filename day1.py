ca0083ce00abb5556723e459ca08bdd7d802a2db
bulela <bulelantutu@gmail.com>
2020-09-22 09:40:10 +0200

    commit

--- day1.py
+++ day1.py
@@ -8,3 +8,10 @@ class_students = "19"
 
 print("There are " + class_students + " students in " + class_name)
 
+student_name = input("what is your name: ")
+
+print("So your name is " + student_name)
+
+student_age = int(input("How old are you: "))
+
+print("so in 10 years you'll be")
---
+++ day1_exercise.py
@@ -0,0 +1,8 @@
+name = input("type your name\n")
+h= "hello "
+g= " how are you ?"
+print(h + name + g)
+
+radius=int(input("enter radius"))
+circumference=2*radius*3.1415
+print(circumference )
